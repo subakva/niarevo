43eba6d517a453d19b0e193431c0d4fd60dbef46
Jason Wadsworth <jdwadsworth@gmail.com>
2009-11-22 22:51:04 -0500

    do not require data-related gems in test environments

--- config/gemtronics.rb
+++ config/gemtronics.rb
@@ -7,16 +7,19 @@ group(:default) do |g|
   g.add('will_paginate', :version => '2.3.11')
   g.add('gravtastic', :version => '2.1.3')
   g.add('rdiscount', :version =>'1.5.5')
-  g.add('json', :version =>'1.2.0')
+end
+
+group(:data_loading) do |g|
   g.add('matthuhiggins-foreigner', :version => '0.3.1', :require => 'foreigner')
+  g.add('json', :version =>'1.2.0')
 end
 
-group(:production, :dependencies => :default) do |g|
-  
+group(:production, :dependencies => [:default,:data_loading]) do |g|
+
 end
 
-group(:development, :dependencies => :default) do |g|
-  
+group(:development, :dependencies => [:default,:data_loading]) do |g|
+
 end
 
 group(:test, :dependencies => :development, :load => false) do |g|
