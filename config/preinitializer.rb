bc46e08f17c4a287430791a0ba3022a74bd6956a
Jason Wadsworth <jdwadsworth@gmail.com>
2009-11-25 00:34:18 -0600

    Changes to fix preinitializer in production

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,3 +1,8 @@
+if ENV['RAILS_ENV'] == 'production'
+  ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
+  RAILS_ENV = 'production'
+end
+  
 require 'rubygems'
 require 'gemtronics'
 
