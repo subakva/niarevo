8799e7d3782aa9923c1fc2885e99c23874888f54
Jason Wadsworth <jdwadsworth@gmail.com>
2009-11-27 14:54:11 -0600

    Adding a default session key

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -10,5 +10,5 @@ configatron.hoptoad.api_key = ''
 configatron.recaptcha.public_key = ''
 configatron.recaptcha.private_key = ''
 configatron.smtp.password = ''
-configatron.session.secret = ''
+configatron.session.secret = 'dead60b4dab26a286723bb5da73d51bb0073ca5ecf2774f7c94d2fd313034b48171d77d8a0f4487f0773a690e58a3bccefac1d26c079bd567dbf346f9e74984d'
 
