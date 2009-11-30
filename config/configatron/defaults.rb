911a27d698433ebd14f55aac29fa0dec4c946fd6
Jason Wadsworth <jdwadsworth@gmail.com>
2009-11-29 22:58:56 -0500

    Added some code to test FB Connect

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -5,10 +5,12 @@ configatron.notifier.auto_cc = 'outgoing@dreamtagger.com'
 configatron.asset_hosts.asset_host_url = "dreamtagger.local:3000"
 
 # from credentials.rb
-configatron.newrelic.license_key = ''
-configatron.hoptoad.api_key = ''
-configatron.recaptcha.public_key = ''
-configatron.recaptcha.private_key = ''
-configatron.smtp.password = ''
+configatron.newrelic.license_key = 'configatron.newrelic.license_key'
+configatron.hoptoad.api_key = 'configatron.hoptoad.api_key'
+configatron.recaptcha.public_key = 'configatron.recaptcha.public_key'
+configatron.recaptcha.private_key = 'configatron.recaptcha.private_key'
+configatron.smtp.password = 'configatron.smtp.password'
 configatron.session.secret = 'dead60b4dab26a286723bb5da73d51bb0073ca5ecf2774f7c94d2fd313034b48171d77d8a0f4487f0773a690e58a3bccefac1d26c079bd567dbf346f9e74984d'
+configatron.fb_connect.api_key = 'configatron.fb_connect.api_key'
+configatron.fb_connect.secret = 'configatron.fb_connect.secret'
 
