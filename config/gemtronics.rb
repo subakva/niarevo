72a12da53297da3a8e4e4ef664f0dae2ce937c94
Jason Wadsworth <jdwadsworth@gmail.com>
2009-12-02 23:13:14 -0500

    upgraded gems

--- config/gemtronics.rb
+++ config/gemtronics.rb
@@ -2,8 +2,8 @@ group(:default) do |g|
   g.add('gemtronics', :version => '>=0.7.2')
   g.add('rails', :version => '2.3.5')
   g.add('is_taggable', :version => '0.1.0')
-  g.add('authlogic', :version => '2.1.2')
-  g.add('searchlogic', :version => '2.3.6')
+  g.add('authlogic', :version => '2.1.3')
+  g.add('searchlogic', :version => '2.3.9')
   g.add('will_paginate', :version => '2.3.11')
   g.add('gravtastic', :version => '2.1.3')
   g.add('rdiscount', :version =>'1.5.5')
@@ -34,10 +34,12 @@ group(:test, :dependencies => :development, :load => false) do |g|
   g.add('jscruggs-metric_fu', :version => '1.1.5')
   g.add('bmabey-email_spec', :version => '0.3.4', :require => 'email_spec')
   g.add('fakeweb', :version => '1.2.7')
+  g.add('roodi', :version => '2.0.1')
+  g.add('reek', :version => '1.2.6')
 end
 
 group(:cucumber, :dependencies => :test, :load => false) do |g|
-  g.add('cucumber', :version => '0.4.3')
-  g.add('webrat', :version => '0.5.3')
+  g.add('cucumber', :version => '0.4.4')
+  g.add('webrat', :version => '0.6.0')
   g.add('nokogiri', :version => '1.4.0')
 end
