8edd5ff89014f70e75e582af55e81cb927848082
Jason Wadsworth <jdwadsworth@gmail.com>
2013-05-11 00:39:29 -0700

    Extracted form helpers into a class

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,43 +1,22 @@
 module FormHelper
   def dt_text_area(form, symbol, options = nil, &block)
-    dt_form_field(form, :text_area, symbol, options, &block)
+    DreamTaggerControl.new(form, :text_area, symbol, options).render(self, &block)
   end
 
   def dt_text_field(form, symbol, options = nil, &block)
-    dt_form_field(form, :text_field, symbol, options, &block)
+    DreamTaggerControl.new(form, :text_field, symbol, options).render(self, &block)
   end
 
   def dt_email_field(form, symbol, options = nil, &block)
-    dt_form_field(form, :email_field, symbol, options, &block)
+    DreamTaggerControl.new(form, :email_field, symbol, options).render(self, &block)
   end
 
   def dt_password_field(form, symbol, options = nil, &block)
-    dt_form_field(form, :password_field, symbol, options, &block)
+    DreamTaggerControl.new(form, :password_field, symbol, options).render(self, &block)
   end
 
   def dt_check_box(form, symbol, options = nil, &block)
-    dt_form_field(form, :check_box, symbol, options, &block)
-  end
-
-  def dt_form_field(form, input_method, symbol, options = nil, &block)
-    options ||= {}
-    options = {
-      value: nil,
-      label: symbol.to_s.humanize
-    }.merge(options)
-    errors = form.object.errors
-    group_classes = errors.present? ? 'control-group error' : 'control-group'
-    content_tag(:div, class: group_classes) do
-      label_html = form.label(symbol, options[:label], class: 'control-label' )
-      control_html = content_tag(:div, class: 'controls') do
-        controls_parts = []
-        controls_parts << invoke_input_method(form, input_method, symbol, options)
-        controls_parts << dt_field_errors(errors, symbol)
-        controls_parts << capture(&block) if block_given?
-        controls_parts.join('').html_safe
-      end
-      [label_html, control_html].join('').html_safe
-    end
+    DreamTaggerControl.new(form, :check_box, symbol, options).render(self, &block)
   end
 
   def dt_submit(form, button_text, &block)
@@ -56,22 +35,5 @@ module FormHelper
     content_tag(:a, href: href, class: 'btn btn-help', data: {toggle: 'modal'}) do
       content_tag(:i, '', class: 'icon-question-sign')
     end
-  end
-
-  def dt_field_errors(errors, symbol)
-    error_content = []
-    if errors[symbol]
-      errors[symbol].each do |error|
-        message = errors.full_message(symbol, error)
-        error_content << content_tag(:span, message, class:'help-inline')
-      end
-    end
-    error_content.join('').html_safe
-  end
-
-  def invoke_input_method(form, input_method, symbol, options)
-    input_method_params = [input_method, symbol]
-    input_method_params << options.slice(:value) if options[:value].present?
-    form.send(*input_method_params)
   end
 end
---
+++ app/renderers/dream_tagger_control.rb
@@ -0,0 +1,59 @@
+class DreamTaggerControl
+  attr_reader :form, :input_method, :symbol, :options
+
+  def initialize(form, input_method, symbol, options = nil)
+    @form, @input_method, @symbol = form, input_method, symbol
+    @options = {
+      value: nil,
+      label: symbol.to_s.humanize
+    }.merge(options || {})
+  end
+
+  def render(view, &block)
+    view.content_tag(:div, class: group_classes) do
+      [
+        render_label(view),
+        render_controls_element(view, &block)
+      ].join('').html_safe
+    end
+  end
+
+  def render_controls_element(view, &block)
+    view.content_tag(:div, class: 'controls') do
+      controls_parts = []
+      controls_parts << render_input_element
+      controls_parts << render_field_errors(view)
+      controls_parts << view.capture(&block) if block_given?
+      controls_parts.join('').html_safe
+    end
+  end
+
+  def render_field_errors(view)
+    error_content = []
+    if errors[symbol]
+      errors[symbol].each do |error|
+        message = errors.full_message(symbol, error)
+        error_content << view.content_tag(:span, message, class:'help-inline')
+      end
+    end
+    error_content.join('').html_safe
+  end
+
+  def render_label(view)
+    form.label(symbol, options[:label], class: 'control-label')
+  end
+
+  def render_input_element
+    input_method_params = [input_method, symbol]
+    input_method_params << options.slice(:value) if options[:value].present?
+    form.send(*input_method_params)
+  end
+
+  def errors
+    @errors ||= form.object.errors
+  end
+
+  def group_classes
+    errors.present? ? 'control-group error' : 'control-group'
+  end
+end
