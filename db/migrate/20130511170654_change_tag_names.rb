35a873f99d1ab3121a9e7b372725eed4bf61432e
Jason Wadsworth <jdwadsworth@gmail.com>
2013-05-18 18:59:41 -0700

    In Rails 4, the indexes are renamed with rename_column

--- db/migrate/20130511170654_change_tag_names.rb
+++ db/migrate/20130511170654_change_tag_names.rb
@@ -3,12 +3,6 @@ class ChangeTagNames < ActiveRecord::Migration
     rename_column :dreams, :context_tag_count, :dreamer_tag_count
     rename_column :dreams, :content_tag_count, :dream_tag_count
 
-    remove_index :dreams, :content_tag_count
-    add_index :dreams, :dream_tag_count
-
-    remove_index :dreams, :context_tag_count
-    add_index :dreams, :dreamer_tag_count
-
     execute %{UPDATE taggings SET context = 'dreamer_tags' WHERE context = 'context_tags'}
     execute %{UPDATE taggings SET context = 'dream_tags'   WHERE context = 'content_tags'}
   end
@@ -16,12 +10,6 @@ class ChangeTagNames < ActiveRecord::Migration
   def down
     rename_column :dreams, :dreamer_tag_count, :context_tag_count
     rename_column :dreams, :dream_tag_count, :content_tag_count
-
-    add_index :dreams, :content_tag_count
-    remove_index :dreams, :dream_tag_count
-
-    add_index :dreams, :context_tag_count
-    remove_index :dreams, :dreamer_tag_count
 
     execute %{UPDATE taggings SET context = 'context_tags' WHERE context = 'dreamer_tags'}
     execute %{UPDATE taggings SET context = 'content_tags' WHERE context = 'dream_tags'}
